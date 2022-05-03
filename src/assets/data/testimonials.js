78debec23a66c207d918e58a3775946381b489bc
Demilade Folarin <demiladefolarin@gmail.com>
2022-05-03 16:46:06 +0100

    final commit deploy_net

--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -1,25 +1,25 @@
 const testimonials = [
   {
     id: 1,
-    name: 'Shaif Arfan',
+    name: 'Abisade Folarin',
     title: 'CEO',
-    org: 'WEB CIFAR',
+    org: 'iQentertainment',
     desc:
-      'Ayan is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+      'Demilade is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
   },
   {
     id: 2,
-    name: 'Fahim Elahe',
-    title: 'Co-founder',
-    org: 'Kreetive',
+    name: 'Morolade Folarin',
+    title: 'CEO',
+    org: 'iQentertainment',
     desc:
       'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
   },
   {
     id: 3,
-    name: 'Sayed',
-    title: 'Marketing Head',
-    org: 'Eleven Dawn',
+    name: 'Toyeeb Adepoju',
+    title: 'CEO / Founder',
+    org: 'Tweeby FC',
     desc:
       'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
   },
